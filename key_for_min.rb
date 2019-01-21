c7b8250a9a95334a2aff86f0ed85d6b74df1fa57
Brian Haltiwanger <bhalt@bellsouth.net>
2019-01-21 15:01:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,10 +6,10 @@ def key_for_min_value(name_hash)
   current_key = nil
   name_hash.each do |name_key, name_value|
     if current_value = []
+      binding.pry
       current_value = name_value
       current_key = name_key
     else
-      binding.pry
       if current_value > name_value
         current_value = name_value
         current_key = name_key
